9cf18792ea279d43507ee4754145f5872d362dfd
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-04-05 11:27:22 -0400

    Commit con modelo MVC

---
+++ MVC1/MVC1/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace MVC1.Models
+{
+    public class DataContext:DbContext
+    {
+       
+            public DataContext():base("DefaultConnection")
+            {
+
+            }
+
+        public System.Data.Entity.DbSet<MVC1.Models.Product> Products { get; set; }
+    }
+}
