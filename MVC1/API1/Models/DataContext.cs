3c39868313d9ad814156edb83a4cd813a99dd5f1
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-04-05 12:34:22 -0400

    Commit con modelo API

---
+++ MVC1/API1/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace API1.Models
+{
+    public class DataContext:DbContext
+    {
+       
+            public DataContext():base("DefaultConnection")
+            {
+
+            }
+
+        public System.Data.Entity.DbSet<API1.Models.Product> Products { get; set; }
+    }
+}
